8947e4ea367c5f20fbff585e665f7c43900dd705
Pavel <paulsc@mail.ru>
2020-01-10 15:32:08 +0300

    Version 3
    add new Imgbutton

--- app/src/main/java/ru/planp/gittest/MainActivity.java
+++ app/src/main/java/ru/planp/gittest/MainActivity.java
@@ -14,19 +14,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void onClick_Button(View view) {
+        TextView helloTextView = findViewById(R.id.textView);
+        helloTextView.setText("TextButton");
+    }
+
     public void onClick_IButton(View view) {
         TextView helloTextView = findViewById(R.id.textView);
         helloTextView.setText("Image Button");
     }
 
-    public void onClick_Button(View view) {
-        TextView helloTextView = findViewById(R.id.textView);
-        helloTextView.setText("Button");
-    }
-
     public void onClick_IButton2(View view) {
         TextView helloTextView = findViewById(R.id.textView);
-        helloTextView.setText("Button");
+        helloTextView.setText("Image Button2");
+    }
+
+    public void onClick_IButton3(View view) {
+        TextView helloTextView = findViewById(R.id.textView);
+        helloTextView.setText("Image Button3");
     }
 
 }
