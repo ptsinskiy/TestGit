dbedaa30cb6a5ee7156b41ef9e3ea9d3f194fedb
Pavel <paulsc@mail.ru>
2020-01-10 13:13:22 +0300

    Version 2
    add new button

--- app/src/main/java/ru/planp/gittest/MainActivity.java
+++ app/src/main/java/ru/planp/gittest/MainActivity.java
@@ -3,6 +3,8 @@ package ru.planp.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick_IButton(View view) {
+        TextView helloTextView = findViewById(R.id.textView);
+        helloTextView.setText("Image Button");
+    }
+
+    public void onClick_Button(View view) {
+        TextView helloTextView = findViewById(R.id.textView);
+        helloTextView.setText("Button");
+    }
+
+    public void onClick_IButton2(View view) {
+        TextView helloTextView = findViewById(R.id.textView);
+        helloTextView.setText("Button");
+    }
+
 }
